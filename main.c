729be40ce9138feee096e8edd4e04b9d48ddc57d
StanaAndrei <stadey33@gmail.com>
2026-07-26 00:54:42 +0300

    defined struct

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#define CIDR_ADDR_MAX_STR_LEN 20
+
+typedef struct {
+	uint32_t ip;
+	uint8_t prefix;
+} cidr_addr_t;
+
+cidr_addr_t cidr_addr_from_str(const char cidr_addr_str[CIDR_ADDR_MAX_STR_LEN]) {
+	
+}
+
+void compute_subnets() {
+
+}
 
 int main(void) {
-	puts("Hi!");
+	char input[] = "192.168.32.19/28";
+	puts(input);
+	//cidr_addr_t caddr = cidr_addr_from_str(input);
+
 	return 0;
 }
